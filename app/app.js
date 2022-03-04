3d695e7be2e5b0b6b0386c95b320cb0dafd4de5b
ZoePlayoust <playoustz@gmail.com>
2022-03-04 11:23:30 +0100

    Improving Js code
    Revoming double function and created a more efficient path

--- app/app.js
+++ app/app.js
@@ -46,99 +46,55 @@ let month = months[now.getMonth()];
 let date = document.querySelector("#date");
 date.innerHTML = `${day}, ${month}/${number}`;
 
-// City
-function changeCity(event) {
-  let cityTyped = document.querySelector("#city-form");
-  let cityToChange = document.querySelector("#city");
-  cityToChange.innerHTML = cityTyped.value;
-
-  let apiKey = "0c1a639b4888a93ab5ae1ed2074d5083";
-  let unitMetric = "metric";
-  let city = cityTyped.value;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${unitMetric}&appid=${apiKey}`;
-
-  axios.get(apiUrl).then(showTemperature);
-}
-
-let selectCity = document.querySelector("#search");
-selectCity.addEventListener("click", changeCity, showTemperature);
-
-// Change Temperature unit
-
-function changeToFahrenheit(celsius) {
-  let temperature = document.querySelector("#temperature-variable");
-
-  temperature.innerHTML = 64;
-}
-
-let selectFahreinheit = document.querySelector("#fahrenheit");
-selectFahreinheit.addEventListener("click", changeToFahrenheit);
-
-function changeToCelsius(fahreinheit) {
-  let temperature = document.querySelector("#temperature-variable");
-  temperature.innerHTML = 18;
-}
-
-let selectCelsius = document.querySelector("#celsius");
-selectCelsius.addEventListener("click", changeToCelsius);
-
 //Side Dates to loop still a mess atm but it will look better
 
-// Temperature
-
-function showTemperature(response) {
-  let temperature = Math.round(response.data.main.temp);
-  let temperatureToShow = document.querySelector("#temperature-variable");
-  temperatureToShow.innerHTML = temperature;
-
-  setDescription(
-    response.data.weather[0].description,
-    document.querySelector("#description")
-  );
-
-  setHumidity(response.data.main.humidity, document.querySelector("#humidity"));
-
-  setWind(response.data.wind.speed, document.querySelector("#windspeed"));
+// City
+function submitForm(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-form").value;
+  searchCity(city);
+  console.log(city);
 }
+function searchCity(city) {
+  let apiKey = "0c1a639b4888a93ab5ae1ed2074d5083";
+  let unitMetric = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${unitMetric}&appid=${apiKey}`;
+  axios.get(apiUrl).then(showWeather);
+}
+let selectCity = document.querySelector("#search");
+selectCity.addEventListener("click", submitForm);
+
+// Change Temperature unit -to be done
 
 //Current
-
 function retrievePosition(position) {
   let apiKey = "0c1a639b4888a93ab5ae1ed2074d5083";
   let lat = position.coords.latitude;
   let lon = position.coords.longitude;
   let unitMetric = "metric";
   let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=${unitMetric}&appid=${apiKey}`;
-  axios.get(url).then(showCurrentTemperature);
+  axios.get(url).then(showWeather);
 }
 function getCoords(event) {
   navigator.geolocation.getCurrentPosition(retrievePosition);
 }
-function showCurrentTemperature(response) {
-  let temperature = Math.round(response.data.main.temp);
-  let temperatureToShow = document.querySelector("#temperature-variable");
-  temperatureToShow.innerHTML = temperature;
 
-  let city = document.querySelector("#city");
-  city.innerHTML = response.data.name;
+// Display Weather
 
-  setDescription(
-    response.data.weather[0].description,
-    document.querySelector("#description")
+function showWeather(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature-variable").innerHTML = Math.round(
+    response.data.main.temp
   );
-  setHumidity(response.data.main.humidity, document.querySelector("#humidity"));
-  setWind(response.data.wind.speed, document.querySelector("#windspeed"));
+  document.querySelector("#humidity").innerHTML =
+    response.data.main.humidity + " %";
+  document.querySelector("#windspeed").innerHTML =
+    Math.round(response.data.wind.speed * 3.6) + " km/h";
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].description;
 }
 
 let selectCurrent = document.querySelector("#current");
 selectCurrent.addEventListener("click", getCoords);
 
-function setDescription(description, element) {
-  element.innerHTML = description;
-}
-function setHumidity(humidity, element) {
-  element.innerHTML = humidity + " %";
-}
-function setWind(wind, element) {
-  element.innerHTML = Math.round(wind * 3.6) + " km/h";
-}
+searchCity("Arles");
